b9995b61bcd3686206b1a13048159c638d63a663
Nahom <nahomdebele002@gmail.com>
2023-10-30 00:20:57 +0300

    added module federation with ecommerce app i build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "orderApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        './Orders': './src/pages/Orders',
+        './Line': './src/pages/Charts/Line'
+
+      },
+      shared: ['react','react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
